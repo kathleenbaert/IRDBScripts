82d62cc587b15084da8a344964df1b3f657b07f1
Kathleen Baert <kathleen.baert@marquette.edu>
2018-03-05 08:14:43 -0600

    took out 2d array, before clean up

--- src/scripts/SemesterRegistration.java
+++ src/scripts/SemesterRegistration.java
@@ -22,30 +22,36 @@ public class SemesterRegistration {
 	public static XSSFRow row1MainSheet, row1EdittedData;
 	public static XSSFSheet mainSheet, edittedData;
 	public static String[] firstRowMainSheet, firstRowEdittedData;
-	public static String[][] wholeSheet;
 
 	public static void main(String[] args) {
 
 		try {
 			XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream("IRAbridged.xlsx"));
-		    workbook.setMissingCellPolicy(MissingCellPolicy.CREATE_NULL_AS_BLANK);
-
+			workbook.setMissingCellPolicy(MissingCellPolicy.CREATE_NULL_AS_BLANK);
+			workbook.setMissingCellPolicy(Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
 			mainSheet = workbook.getSheetAt(0);
 			edittedData = workbook.createSheet("edittedData");
 
-			setupMainSheet(mainSheet);
+			//setupMainSheet(mainSheet);
 
 			setUpEdittedData(edittedData);
-
-			int typeIndex = Arrays.asList(firstRowMainSheet).indexOf("C_Type");
+			//int typeIndex = Arrays.asList(firstRowMainSheet).indexOf("C_Type");
 			for (int i = 0; i < mainSheet.getPhysicalNumberOfRows(); i++) {
-				if (isACoOp(i, typeIndex) || isAnInternship(i, typeIndex) || isResearch(i, typeIndex) ||isPartTimeWork(i, typeIndex)) {
-					//works
-					//System.out.println("Row " + i + "is yes");
-				}
-				else {
+				if (isACoOp(i, 5	) || isAnInternship(i, 5) || isResearch(i, 5)
+						|| isPartTimeWork(i, 5)) {
+					System.out.println("works");
 
-				
+
+					// works
+					// System.out.println("Row " + i + "is yes");
+					// find what registration class they need the pair for
+					// works
+					//int regIndex = Arrays.asList(firstRowMainSheet).indexOf("RegCode");
+					//String regNum = wholeSheet[i][regIndex];
+					//System.out.println(i + " " + regIndex + " " + regNum);
+
+				} else {
+					System.out.println("not a coop");
 				}
 			}
 
@@ -62,26 +68,13 @@ public class SemesterRegistration {
 	}
 
 	public static void setupMainSheet(XSSFSheet mainSheet) {
-
-		// get first row:
-		row1MainSheet = mainSheet.getRow(0);
-		firstRowMainSheet = new String[47];// num of columns across
-		for (int i = 0; i < firstRowMainSheet.length; i++) {
-			Cell cell = row1MainSheet.getCell(i);
-			firstRowMainSheet[i] = cell.getRichStringCellValue().getString();
-		}
-		// works
-		// for(int i = 0; i < firstRow.length; i++){
-		// System.out.println(firstRow[i]);
-		// }
-
-		// read in rest of the sheet:
-
+		/*
 		int i = 0, j = 0;
 		wholeSheet = new String[mainSheet.getPhysicalNumberOfRows()][47];
 
 		for (Row row : mainSheet) {
 			for (Cell cell : row) {
+		          int c = row.getCell(cell, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
 
 				switch (cell.getCellTypeEnum().toString()) {
 				case ("STRING"):
@@ -105,55 +98,58 @@ public class SemesterRegistration {
 			i++;
 			j = 0;
 		}
-		// works
-		// System.out.println(Arrays.deepToString(wholeSheet));
+		*/
 	}
-
 	public static void setUpEdittedData(XSSFSheet edittedData) {
 
 		firstRowEdittedData = new String[] { "ID", "MUID", "TERM", "COMPANY_ID", "ACTIVITY", "SALARY", "CITY", "STATE",
 				"COUNTRY", "REGID", "WORK_REG", "WORK_GRADE", "GRADING_REG", "GRADING_GRADE", "EMPLOYER_EVAL_DATE",
 				"EMPLOYER_EVAL", "EMPLOYER_AUTH", "STUDENT_EVAL_DATE", "STUDENT_EVAL", "STUDENT_EVAL_DATE" };
 
-		for(int i = 0; i < firstRowEdittedData.length; i++) {
-            CellReference cr = new CellReference(0, i);
+		for (int i = 0; i < firstRowEdittedData.length; i++) {
+			CellReference cr = new CellReference(0, i);
 			int r = cr.getRow();
 			int c = cr.getCol();
 			row1EdittedData = edittedData.getRow(r);
 			if (row1EdittedData == null)
-			    row1EdittedData = edittedData.createRow(r);
+				row1EdittedData = edittedData.createRow(r);
 			Cell cell = row1EdittedData.getCell(c);
-			cell.setCellValue(firstRowEdittedData[i]);		
+			cell.setCellValue(firstRowEdittedData[i]);
 		}
-		
+
 	}
 
 	public static boolean isACoOp(int row, int col) {
 		// works
 		// System.out.println(wholeSheet[row][col]);
-		if (wholeSheet[row][col].equals("1.0")) {
-			// System.out.println("row " + row + " col " + col + " YES IS A CO OP");
+		if(mainSheet.getRow(row).getCell(col).toString().equals("1.0")) {
+			//System.out.println("row " + row + " col " + col + " YES IS A CO OP");
 			return true;
-		}
-		// System.out.println("row " + row + " col " + col + " NO IS NOT A CO OP");
+		}		
+		//System.out.println(mainSheet.getRow(row).getCell(col).toString());
+		//System.out.println("row " + row + " col " + col + " NO IS NOT A CO OP");
 
 		return false;
 	}
+	
+
 	public static boolean isAnInternship(int row, int col) {
 		// works
 		// System.out.println(wholeSheet[row][col]);
-		if (wholeSheet[row][col].equals("2.0")) {
+		if(mainSheet.getRow(row).getCell(col).toString().equals("2.0")) {
 			// System.out.println("row " + row + " col " + col + " YES IS AN INTERNSHIP");
 			return true;
 		}
-		// System.out.println("row " + row + " col " + col + " NO IS NOT AN INTERNSHIP");
+		// System.out.println("row " + row + " col " + col + " NO IS NOT AN
+		// INTERNSHIP");
 
 		return false;
 	}
+
 	public static boolean isResearch(int row, int col) {
 		// works
 		// System.out.println(wholeSheet[row][col]);
-		if (wholeSheet[row][col].equals("4.0")) {
+		if(mainSheet.getRow(row).getCell(col).toString().equals("4.0")) {
 			// System.out.println("row " + row + " col " + col + " YES IS RESEARCH");
 			return true;
 		}
@@ -161,15 +157,18 @@ public class SemesterRegistration {
 
 		return false;
 	}
+
 	public static boolean isPartTimeWork(int row, int col) {
 		// works
 		// System.out.println(wholeSheet[row][col]);
-		if (wholeSheet[row][col].equals("7.0")) {
+		if(mainSheet.getRow(row).getCell(col).toString().equals("7.0")) {
 			// System.out.println("row " + row + " col " + col + " YES IS PART TIME WORK");
 			return true;
 		}
-		// System.out.println("row " + row + " col " + col + " NO IS NOT PART TIME WORK");
+		// System.out.println("row " + row + " col " + col + " NO IS NOT PART TIME
+		// WORK");
 
 		return false;
 	}
+	
 }
