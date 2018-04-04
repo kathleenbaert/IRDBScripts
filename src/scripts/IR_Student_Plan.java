e5c92d98fb3f9349a0406b5e210a61dd63c21eb3
Kathleen Baert <kathleen.baert@marquette.edu>
2018-04-04 11:44:28 -0500

    fixed student plan, still need to spot check

--- src/scripts/IR_Student_Plan.java
+++ src/scripts/IR_Student_Plan.java
@@ -14,29 +14,33 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class IR_Student_Plan {
-	
+
 	public static XSSFSheet mainSheet, edittedData;
 	public static int edittedDataCurrRow;
-	
-	public static void main(String [] args) {
+
+	public static void main(String[] args) {
 
 		try {
-			//change
-			XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(""));
+			// change
+			XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream("IRAbridged.xlsx"));
 			mainSheet = workbook.getSheetAt(0);
 			edittedData = workbook.createSheet("edittedData");
 			workbook.setMissingCellPolicy(MissingCellPolicy.CREATE_NULL_AS_BLANK);
 			workbook.setMissingCellPolicy(Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-			
+
 			setUpEdittedData();
 			System.out.println("...set up editted data complete");
 			transferLoop();
 			System.out.println("...transfer loop complete");
 
 			cleanUpLoop();
-			
+
 			System.out.println("...clean up loop complete");
 			// write out here
+
+			findDoubles();
+			System.out.println("...finding doubles complete");
+
 			FileOutputStream fileOut = new FileOutputStream("IR_Student_Plan.xlsx");
 			workbook.write(fileOut);
 			fileOut.close();
@@ -50,9 +54,9 @@ public class IR_Student_Plan {
 		}
 
 	}
-	
+
 	public static void setUpEdittedData() {
-		String [] firstRowEdittedData = new String[] { "ID", "MUID", "TERM", "Activity"};
+		String[] firstRowEdittedData = new String[] { "ID", "MUID", "TERM", "ACTIVITY", "REGISTRATION" };
 		for (int i = 0; i < firstRowEdittedData.length; i++) {
 			CellReference cr = new CellReference(0, i);
 			int r = cr.getRow();
@@ -63,40 +67,62 @@ public class IR_Student_Plan {
 			Cell cell = row1EdittedData.getCell(c);
 			cell.setCellValue(firstRowEdittedData[i]);
 		}
-
-		
-		
 	}
-	
-	public static void transferLoop(){
-		for(int i = 1; i < mainSheet.getPhysicalNumberOfRows(); i++) {
-			//CoOpID
+
+	public static void transferLoop() {
+		for (int i = 1; i < mainSheet.getPhysicalNumberOfRows(); i++) {
+			// CoOpID
 			Row row = edittedData.getRow(i);
 			if (row == null) {
 				row = edittedData.createRow(i);
 			}
-			edittedData.getRow(i).getCell(0)
-			.setCellValue(mainSheet.getRow(i).getCell(0).toString());
-			//MUID
-			edittedData.getRow(i).getCell(1)
-			.setCellValue(mainSheet.getRow(i).getCell(1).toString());
-			//Term
-			edittedData.getRow(i).getCell(2)
-			.setCellValue(mainSheet.getRow(i).getCell(2).toString());
-			//Activity
-			edittedData.getRow(i).getCell(3)
-			.setCellValue(mainSheet.getRow(i).getCell(5).toString());
+			edittedData.getRow(i).getCell(0).setCellValue(mainSheet.getRow(i).getCell(0).toString());
+			// MUID
+			edittedData.getRow(i).getCell(1).setCellValue(mainSheet.getRow(i).getCell(1).toString());
+			// Term
+			edittedData.getRow(i).getCell(2).setCellValue(mainSheet.getRow(i).getCell(2).toString());
+			// Activity
+			edittedData.getRow(i).getCell(3).setCellValue(mainSheet.getRow(i).getCell(5).toString());
+			// registration
+			edittedData.getRow(i).getCell(4).setCellValue(mainSheet.getRow(i).getCell(14).toString());
+
 		}
 	}
+
 	public static void cleanUpLoop() {
-		for(int i = 0; i < edittedData.getPhysicalNumberOfRows(); i++) {
-		Replacements r = new Replacements();
-		r.IRKeytoCheckmarqKey(i, 3, edittedData); // works
+		for (int i = 0; i < edittedData.getPhysicalNumberOfRows(); i++) {
+			Replacements r = new Replacements();
+			r.IRKeytoCheckmarqKey(i, 3, edittedData); // works
 
-		r.convertIRStudentActivityPlans(i, edittedData, 3);
+			r.convertIRStudentActivityPlans(i, edittedData, 3);
 
-		}		
+		}
 	}
-	
+
+	public static void findDoubles() {
+		int original = edittedData.getPhysicalNumberOfRows();
+		for (int i = 0; i < original - 1; i++) {
+			// 8 = registration ID
+			if (edittedData.getRow(i).getCell(0).toString().equals(edittedData.getRow(i + 1).getCell(0).toString()) &&
+
+					edittedData.getRow(i).getCell(1).toString().equals(edittedData.getRow(i + 1).getCell(1).toString())
+
+					&& edittedData.getRow(i).getCell(2).toString()
+							.equals(edittedData.getRow(i + 1).getCell(2).toString())
+
+					&& edittedData.getRow(i).getCell(3).toString()
+							.equals(edittedData.getRow(i + 1).getCell(3).toString())) {//ID, MUID, Term, activity all equal
+				// System.out.println(i + " duplicates!");
+				//transfer extra reg codes
+				String s1 = edittedData.getRow(i).getCell(4).toString();
+				String s2 = edittedData.getRow(i+1).getCell(4).toString();
+				edittedData.getRow(i).getCell(4).setCellValue(s1 + ", " + s2);
+				System.out.println("ran " + i);
+				Row r = edittedData.getRow(i + 1);
+				edittedData.removeRow(r);
+				i++;
+			}
+		}
+	}
 
 }
